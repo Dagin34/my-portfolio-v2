acea837b3332addaaef9e5011dc08ae9b274b7bc
dagin34 <dagmawinapoleon02@gmail.com>
2025-12-23 14:39:49 +0300

    Changed the location of the google site verification meta tag

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,4 +4,8 @@
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<meta name="google-site-verification" content="D3g1xWI36x7xyVb-KdVKFShsVyv-h_PZWE4TVwdMeYk" />
+</svelte:head>
+
 {@render children()}
