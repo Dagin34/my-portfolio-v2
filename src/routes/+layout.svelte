3a0a79b1922b0a0f486c1b9abac511c6d14aa3f9
dagin34 <dagmawinapoleon02@gmail.com>
2025-12-24 16:08:57 +0300

    SEO Optimizer suggestions 05

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,13 @@
 
 <svelte:head>
 	<meta name="google-site-verification" content="DQ9GnMhhKWqXE4mmG2OE5E9A4fueQqLlz8q_2eP58WA" />
+	<link rel="icon" href="/icon.png" sizes="96x96" type="image/png">
+  	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+	<link rel="manifest" href="/site.webmanifest" />
+
+	<meta name="author" content="Dagmawi Napoleon Bogale" />
+	<meta name="keywords" content="Dagmawi Napoleon Bogale, dagmawi napoleon bogale, Dagmawi Napoleon, dagmawi napoleon, Ethiopian Developer, ethiopian developer, Ethiopia, Ethiopian Software Engineer, ethiopian software engineer, Developer, developer" />
+	<meta name="theme-color" content="#ff6900" />
 </svelte:head>
 
 {@render children()}
