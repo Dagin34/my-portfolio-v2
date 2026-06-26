d5294593e7de1289862a5677461abe1465cc616d
dagin34 <dagmawinapoleon02@gmail.com>
2026-06-26 11:50:13 +0300

    Vercel Insights

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import "./layout.css";
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 
 	let { children } = $props();
 </script>
